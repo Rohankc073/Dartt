8ebf87af058a607bf9c6ff24859b107361d3b5d6
Rohankc073 <rohankc073@gmail.com>
2024-11-18 15:44:26 +0545

    Thrid

--- 1st.dart
+++ 1st.dart
@@ -1,18 +1,20 @@
-void main(){
-    var name = "Rohan KC ";
-    print(name);
+// void main(){
+//     var name = "Rohan KC ";
+//     print(name);
 
-    int a = 2 ;
-    int b = 30;
+//     int a = 2 ;
+//     int b = 30;
      
-    var c = a+b;
+//     var c = a+b;
+//     print(c);
+
+//      String? fullName;
+//     fullName = "Rohan KC ";
+//     print(fullName);
+// }
+
+void main(){
+    List<int> a = <int>[1,2,3,4];
+    final c =a.reversed;
     print(c);
-
-     String? fullName;
-    fullName = "Rohan KC ";
-    print(fullName);
-}
-
-void main1(){
-   
 }
